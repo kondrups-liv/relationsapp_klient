c003734f6bbed8276d3c5134059d11aab1588060
Liv Kondrup <liv@kondrups.dk>
2025-03-02 18:57:23 +0100

    cleaning up

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class Authentication {
-  User? _user;
-
   Future<UserCredential?> signin(
       {required String email,
       required String password,
@@ -13,11 +11,8 @@ class Authentication {
           .signInWithEmailAndPassword(email: email, password: password);
       return userCredential;
     } on FirebaseAuthException catch (e) {
-      String message = 'Ugyldig email eller adgangskode';
-      print('$message ${e.code}');
       return null;
     } catch (e) {
-      print(e);
       return null;
     }
   }
