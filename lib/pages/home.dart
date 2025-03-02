9792a16b0aefb7ba19414f944591f8debf948cee
Liv Kondrup <liv@kondrups.dk>
2025-03-02 18:55:48 +0100

    added userinfo to home screen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,7 +15,6 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     data = ModalRoute.of(context)!.settings.arguments as Map;
-    print(data);
     UserCredential userCredential = data['userCredential'];
     String email = userCredential.user!.email!;
     Client client = Client(email: email);
@@ -25,6 +24,61 @@ class _HomeState extends State<Home> {
         appBar: AppBar(
           title: Text('Velkommen ${client.email}'),
           backgroundColor: Colors.blueAccent,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'NAVN',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.1,
+                ),
+              ),
+              Text(
+                '123 Test',
+                style: TextStyle(
+                  fontSize: 20,
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'EMAIL',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.1,
+                ),
+              ),
+              Text(
+                '123@test.dk',
+                style: TextStyle(
+                  fontSize: 20,
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'TELEFON',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.1,
+                ),
+              ),
+              Text(
+                '12345678',
+                style: TextStyle(
+                  fontSize: 20,
+                  letterSpacing: 2,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
